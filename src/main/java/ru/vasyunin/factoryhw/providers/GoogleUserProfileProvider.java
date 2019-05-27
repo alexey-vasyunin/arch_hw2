b0a6bea4ca00c52dff3fe78d25bcd9a39eaea28f
Alexey Vasyunin <a.vasyunin@gmail.com>
2019-05-27 16:43:03 +0400

    Initial commit

---
+++ src/main/java/ru/vasyunin/factoryhw/providers/GoogleUserProfileProvider.java
@@ -0,0 +1,22 @@
+package ru.vasyunin.factoryhw.providers;
+
+import ru.vasyunin.factoryhw.providers.UserProfileProvider;
+
+public class GoogleUserProfileProvider implements UserProfileProvider {
+
+    public String getId() {
+        return null;
+    }
+
+    public String getPicture() {
+        return null;
+    }
+
+    public String getGender() {
+        return null;
+    }
+
+    public String getEmail() {
+        return null;
+    }
+}
